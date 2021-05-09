132ca20da7e655962aecefe568269e0dbc2c1bca
Lex-124 <ales.zorc1@gmail.com>
2021-05-09 10:46:17 +0200

    setting up dummy pages

---
+++ pages/events/[id].js
@@ -0,0 +1,9 @@
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>This is the Event Detail Page</h1>
+    </div>
+  );
+};
+
+export default EventDetailPage;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const StartingPage = () => {
+  return (
+    <div>
+      <h1>This is the Home Page</h1>
+    </div>
+  );
+};
+
+export default StartingPage;
