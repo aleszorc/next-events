5e0639a610c43e94330e089d353f93616d8bc1f2
Lex-124 <ales.zorc1@gmail.com>
2021-05-10 20:37:24 +0200

    more react components

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,33 @@
+import Link from 'next/link';
+
 const EventItem = (props) => {
-  return <li></li>;
+  const { title, image, date, location, id } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const formattedAddress = location.replace(', ', '\n');
+  const exploreLink = `/event/${id}`;
+
+  return (
+    <li>
+      <img src={`/${image}`} alt={title} />
+      <div>
+        <div>
+          <h2>{title}</h2>
+        </div>
+        <div>
+          <time>{humanReadableDate}</time>
+        </div>
+        <div>{formattedAddress}</div>
+        <div>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default EventItem;
