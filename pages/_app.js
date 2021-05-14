241ff63859963329e51e9a1d44d1ff40ecb58d1f
Lex-124 <ales.zorc1@gmail.com>
2021-05-14 21:06:25 +0200

    merging head content

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>Next Events</title>
+        <meta
+          name="description"
+          content="Find great events that allow you to network and grow"
+        />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
